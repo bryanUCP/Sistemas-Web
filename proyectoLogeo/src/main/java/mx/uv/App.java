2d03b002879ec313492a5719ae9133bf8ed43d23
bryanUCP <bryanpiq2600@gmail.com>
2021-10-08 21:19:14 -0500

    Modificaciones Proyecto Logeo

--- proyectoLogeo/src/main/java/mx/uv/App.java
+++ proyectoLogeo/src/main/java/mx/uv/App.java
@@ -1,5 +1,7 @@
 package mx.uv;
 
+import static spark.Spark.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,17 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        get("/hola", (rq, rs) -> {
+            System.out.println("Request: " + rq.queryParams("usu") + " " + rq.queryParams("pass"));
+            String msj;
+            if (rq.queryParams("usu").equals("Bryan"))
+                msj = "Bienvenido!" + " " + rq.queryParams("usu");
+            else
+                msj = "Usuario equivocado";
+            //return "Hola get " + msj + " " + "<a href='//127.0.0.1:5500/formulario.html'>regresar</a>";
+            return "Hola get " + msj;
+            
+        });
     }
+
 }
--- proyectoLogeo/validacion.js
+++ proyectoLogeo/validacion.js
@@ -15,27 +15,34 @@ BotonValidar.addEventListener("click", function () {
     console.log("{" + "name:'" + params.get("usu") + "'," + "pass:'" + params.get("pass") + "'}");
     //var params2 = "{" + "name:'" + params.get("nombre") + "'," + "pass:'" + params.get("pass") + "'}";  // forma de equívoca de construir el objeto, ya que le sobran las llaves
     var params3 =  "name:'" + params.get("usu") + "'," + "pass:'" + params.get("pass") + "'";
-    axios.post("http://localhost:4567/holaJson", { "usu": params.get("usu"), pass: params.get("pass") } )
+    /*axios.post("http://localhost:4567/hola?", { "usu": params.get("usu"), pass: params.get("pass") } )
         .then(function (rs) {
             console.log(rs.data);
-            //alert(rs.data)
+            alert(rs.data)
         })
         .catch(function (error) {
             console.log(error);
-        });
+        });*/
 
-        //Validar si los datos son correctos
-
+        //Validar si los datos son correctos y no se dejo ningun campo vacio en el login
+         
+        if(params.get("usu") == "" || params.get("pass")==""){
+            window.alert('No puede dejar campos vacios')
+            console.log('el campo contraseña es obligatorio')
+            evt.preventDefault();
+            return false;
+        }else{
         if(params.get("usu") == "Bryan"){
             if(params.get("pass") == "123456789"){
                 console.log("Si entro a los if´s");
                 window.location.replace('ListaUsuarios.html'); //Me redirige a la pagina que deseo si se cumple la condicion.
+            }else{
+                window.alert('Password incorrecto') 
             }
         }else{
             window.location.replace('Registrarse.html');  //Me redirige a la pagina que deseo si se cumple la condicion.
              
         }
-
-
+       }
 
 });
